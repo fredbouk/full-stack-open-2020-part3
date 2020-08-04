743c52999c03bd5e342ac1e902814d6d97ff9d2e
Fredrik Boukharta <fredbouk@gmail.com>
2020-08-04 11:08:19 +0200

    3.8

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const app = express()
 
 // middleware
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('data', (req, res) => JSON.stringify(req.body))
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
 let persons = [
   {
