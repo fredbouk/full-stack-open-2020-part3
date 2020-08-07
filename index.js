500ebf2ed9ac384d4e6b971cb6fcc9f5ccf71f85
Fredrik Boukharta <fredbouk@gmail.com>
2020-08-07 10:13:51 +0200

    Added cors

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
 // middleware
 app.use(express.json())
+
+app.use(cors())
 
 morgan.token('data', (req, res) => JSON.stringify(req.body))
 
