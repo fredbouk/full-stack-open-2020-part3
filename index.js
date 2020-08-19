fd7940adff503e24ef4dd79698e6b7b80ef1bd5d
Fredrik Boukharta <fredbouk@gmail.com>
2020-08-19 11:18:42 +0200

    3.13

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+
+const Person = require('./models/person')
 
 const app = express()
 
@@ -13,31 +16,10 @@ morgan.token('data', (req, res) => JSON.stringify(req.body))
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
-let persons = [
-  {
-    name: 'Arto Hellas',
-    number: '040-123456',
-    id: 1
-  },
-  {
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-    id: 2
-  },
-  {
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-    id: 3
-  },
-  {
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-    id: 4
-  }
-]
-
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  Person.find({}).then(persons => {
+    res.json(persons)
+  })
 })
 
 app.get('/info', (req, res) => {
@@ -90,7 +72,7 @@ app.post('/api/persons/', (req, res) => {
   res.json(person)
 })
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
