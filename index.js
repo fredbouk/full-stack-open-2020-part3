c252eb1ca0991489ef12853080105643fd51c137
Fredrik Boukharta <fredbouk@gmail.com>
2020-08-21 13:35:56 +0200

    3.17

--- index.js
+++ index.js
@@ -68,6 +68,20 @@ app.post('/api/persons/', (req, res) => {
   })
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const updatePerson = {
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, updatePerson, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
 // handler of requests with unknown endpoint
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
